d31df5007166bb690d620399fe0d58cd2aac352f
lullulalla <jisoo950225@gmail.com>
2024-03-19 17:24:20 +0900

    Merge branch 'master' of https://github.com/lullululla/FLEX.git

--- Flex-3/src/main/java/com/example/demo/controller/TestController.java
+++ Flex-3/src/main/java/com/example/demo/controller/TestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 	@GetMapping("/")
 	public String TestController() {
+		System.out.println("수정됨ㅇㅇㅇ");
 		return "...";
 	}
 	
