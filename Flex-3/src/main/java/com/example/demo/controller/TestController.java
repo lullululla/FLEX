1b37d34ce8a0e393ec094100450c39059170ecd7
xo-hyun <xo_hyunn@naver.com>
2024-03-19 17:25:49 +0900

    edit

--- Flex-3/src/main/java/com/example/demo/controller/TestController.java
+++ Flex-3/src/main/java/com/example/demo/controller/TestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 	@GetMapping("/")
 	public String TestController() {
+		System.out.println("hhhh");
 		return "...";
 	}
 	
